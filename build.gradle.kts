02d418c7447f1a3500d9f7d25251a637af2a347f
minofile <minofile@gmail.com>
2026-09-09 17:00:05 +0330

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
 android {
     namespace = "com.sabztar.app"
     compileSdk = 35
@@ -21,4 +26,18 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+}
+
+dependencies {
+
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.activity:activity-compose:1.9.0")
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
