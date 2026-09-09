199b56778ca8d5d2c372777f7770545b5592a104
minofile <minofile@gmail.com>
2026-09-09 16:49:49 +0330

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,24 @@
-plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+android {
+    namespace = "com.sabztar.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.sabztar.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
